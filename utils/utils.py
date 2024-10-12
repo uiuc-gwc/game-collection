73181ed9dcd838c01c65433c444f1386e8785bb0
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-10-12 17:49:48 -0500

    Move playAgain() to utils function

--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,16 @@ def getEnglishWordList():
     with open("utils/dwyl-english-words.txt") as file:
         lines = [line.rstrip() for line in file]
     return lines
+
+def playAgain():
+    validInput = False
+    while not validInput:
+        playagain = input("\n\nThat was fun! play again? (y/n)\n")
+        if playagain.lower() == "yes" or playagain.lower() == "y":
+            validInput = True
+            return True
+        elif playagain.lower() == "no" or playagain.lower() == "n":
+            validInput = True
+            return False
+        else: 
+            print("Please enter a valid answer.")
