e65abd6840c8c82fe28662e89039e89280dd28de
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-10-13 10:49:17 -0500

    wordle files from other branch

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,35 @@
-def getEnglishWordList():
-    with open("utils/dwyl-english-words.txt") as file:
-        lines = [line.rstrip() for line in file]
+from colorama import init as colorama_init
+from colorama import Fore, Back, Style
+
+def getEnglishWordList(length=None, common=True):
+    if common:
+        wordsFile = "utils/google-10000-usa-common.txt"
+    else:
+        wordsFile = "utils/dwyl-english-words.txt"
+    with open(wordsFile) as file:
+        lines = [line.strip() for line in file \
+                 if (length == None or len(line.strip()) == length)]
     return lines
+
+def colorText(text, color):
+    # print in the terminal but have the text colored
+    # color is a string that is one of the following:
+    # "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+    colorama_init()
+    # match the input color string to the colorama colors
+    styles = {
+        "red" : Fore.RED,
+        "green": Fore.GREEN,
+        "yellow" : Fore.YELLOW,
+        "blue" : Fore.BLUE,
+        "magenta": Fore.MAGENTA,
+        "cyan" : Fore.CYAN,
+        "white" : Fore.WHITE,
+    }
+
+    # actually apply the colors. 
+    return f"{styles[color]}{text}{Style.RESET_ALL}"
+
 
 def playAgain():
     validInput = False
